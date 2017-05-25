8355ef9a0f04adde8bf8673559614928d75e796c
Tim Huddle <blazhen88@gmail.com>
2017-05-25 14:36:38 -0400

    can build vendor bundle, now just need to use it in main webpack config

--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -2,15 +2,41 @@
 
 import Webpack from 'webpack'
 import { sync } from 'glob'
+import { resolve } from 'path'
 
 export default {
   devtool: 'cheap-module-source-map',
   output: {
-    path: '.',
+    path: resolve('.'),
     filename: '[name]-dll.js',
     library: '[name]-[hash]-dll'
   },
   entry: {
-    vendor: sync('./bower_components/**/*.{js, purs}')
-  }
+    vendor: sync('bower_components/**/*.{js,purs}').map(p => resolve('.', p))
+  },
+  module: {
+    rules: [{
+      test: /\.purs$/,
+      use: [{
+        loader: 'purs-loader',
+        options: {
+          src: ['bower_components/purescript-*/src/**/*.purs'],
+          bundle: false,
+          psc: 'psa',
+          pscIde: false,
+          warnings: false
+        }
+      }]
+    }]
+  },
+  resolve: {
+    modules: ['node_modules', 'bower_components'],
+    extensions: ['.purs', '.js']
+  },
+  plugins: [
+    new Webpack.DllPlugin({
+      path: '[name]-manifest.json',
+      name: '[name]-[hash]_dll'
+    })
+  ]
 }
